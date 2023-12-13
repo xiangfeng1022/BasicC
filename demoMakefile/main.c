a93a47b9084bb17423228c0155fe903773d0f735
xiangfeng1022 <1365725301@qq.com>
2023-12-13 18:29:59 +0800

    [新增]makefile

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int printString()
-{
-    int ret = 0;
-    printf("print string\n");
-    return ret;
-}
-
 int main()
-{   
-    int num1 = 10;    
-    printf("num1:%d\n", num1);
-    printString();
+{
+    printf("hello world\n");
     return 0;
 }
