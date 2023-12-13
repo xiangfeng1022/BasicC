495e41947a950c29b1c39c72612693a59de45334
xiangfeng1022 <1365725301@qq.com>
2023-12-13 15:27:49 +0800

    [新增]指针和字符串

---
+++ pointer/demoPointerQues.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{   
+    /* 指针和字符串 */
+    char *ptr = "hello world";
+     
+    /* 字符串常量? 什么叫字符串常量 */
+    /* 打印字符串的每一个字符 
+       1、使用for循环的方式*/
+#if 0  
+    //错误的
+    int len = strlen(ptr);
+    printf("len:%d\n", len); 
+    char p = '0';
+    for(int idx = 0; idx < strlen(ptr); idx++)
+    {
+        p = *ptr;
+        printf("p:%c, idx:%d\n", p, idx);
+        ptr++;
+    }
+#endif
+
+#if 0
+    int len = strlen(ptr);
+    printf("len:%d\n", len);
+     char p = '0';
+    for(int idx = 0; idx < len; idx++)
+    {
+        p = *ptr;
+        printf("p:%c, idx:%d\n", p, idx);
+        ptr++;
+    }
+#endif
+
+#if 0 
+    /* 实现方式2: */
+    while (*ptr != '\0')
+    {   
+        printf("%c\t", *ptr);
+        ptr++;
+    }
+#endif
+
+    char *ptr2 = NULL;
+#if 1
+    ptr2 = "zhangsan";
+#else
+    strcpy(ptr2, "zhangsan"); 
+#endif
+
+    printf("ptr2:%s\n", ptr2);
+    
+    return 0;
+}
